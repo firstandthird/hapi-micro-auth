51e3a6255dc449074e4e1fadb427e7bc96e1199c
Eric Willis <eric@ecwillis.com>
2017-12-31 19:12:40 +0000

    Updating to hapi-17

---
+++ test/plugin.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const code = require('code');
+
+const Hapi = require('hapi');
+
+lab.experiment('plugin registration', () => { 
+  lab.test('it should register if passed valid options', async() => {
+    const server = new Hapi.Server();
+    try {
+      await server.register({
+        plugin: require('../'),
+        options: {
+          host: 'http://localhost:8081/auth',
+          routes: true
+        }
+      });
+    } catch (e) {
+      code.fail('Registration should not fail.');
+    }
+    code.expect(server.microauth).to.be.an.object();
+    code.expect(server.microauth.getMe).to.be.a.function();
+    code.expect(server.microauth.getUser).to.be.a.function();
+    code.expect(server.microauth.setCookie).to.be.a.function();
+    code.expect(server.microauth.getTokenFromRequest).to.be.a.function();
+    code.expect(server.microauth.userCache).to.be.an.object();
+
+  });
+
+  lab.test('it should throw an error if host is not set', async() => {
+    const server = new Hapi.Server();
+    await code.expect(server.register(require('../'))).to.reject();
+  });
+});
---
+++ test/server.test.js
@@ -0,0 +1,251 @@
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const code = require('code');
+
+const Hapi = require('hapi');
+
+lab.experiment('server actions', () => {
+  let authServer;
+
+  lab.before(async() => {
+    authServer = new Hapi.Server({ port: 8081 });
+    authServer.route({
+      path: '/api/me/{token}',
+      method: 'get',
+      handler(r, h) {
+        code.expect(r.params.token).to.equal('aToken');
+        return { _id: '1234' };
+      }
+    });
+
+    authServer.route({
+      path: '/api/users/{token}',
+      method: 'get',
+      handler(r, h) {
+        code.expect(r.params.token).to.equal('anotherToken');
+        return { _id: '5678' };
+      }
+    });
+
+    await authServer.start();
+  });
+  lab.after(async() => {
+    await authServer.stop();
+  });
+
+  lab.test('it should protect routes', async() => {
+    const server = new Hapi.Server({ port: 8082, debug: { log: [ '*' ] } });
+    await server.register({
+      plugin: require('../'),
+      options: {
+        host: 'http://localhost:8081'
+      }
+    });
+
+    server.route({
+      path: '/main',
+      method: 'get',
+      handler(r, h) {
+        code.fail('this should be protected');
+        return 'nothing here';
+      }
+    });
+
+    server.route({
+      path: '/main-two',
+      method: 'get',
+      handler(r, h) {
+        return 'it is okay';
+      }
+    });
+
+    await server.start();
+
+    const result = await server.inject({ url: '/main' });
+    code.expect(result.statusCode).to.equal(302);
+
+    const resultTwo = await server.inject({ url: '/main-two?token=aToken' });
+    code.expect(resultTwo.statusCode).to.equal(200);
+
+    await server.stop();
+  });
+
+  lab.test('it should protect routes without cache enabled', async() => {
+    const server = new Hapi.Server({ port: 8082, debug: { log: [ '*' ] } });
+    await server.register({
+      plugin: require('../'),
+      options: {
+        host: 'http://localhost:8081',
+        routes: true,
+        cacheEnabled: false
+      }
+    });
+
+    server.route({
+      path: '/main',
+      method: 'get',
+      handler(r, h) {
+        code.fail('this should be protected');
+        return 'nothing here';
+      }
+    });
+    
+    server.route({
+      path: '/main-two',
+      method: 'get',
+      handler(r, h) {
+        return 'it is okay';
+      }
+    });
+
+    await server.start();
+
+    const result = await server.inject({ url: '/main' });
+    code.expect(result.statusCode).to.equal(302);
+    
+    const resultTwo = await server.inject({ url: '/main-two?token=aToken' });
+    code.expect(resultTwo.statusCode).to.equal(200);
+
+    await server.stop();
+
+  });
+  
+  lab.test('it should support try mode', async() => {
+    const server = new Hapi.Server({ port: 8082, debug: { log: [ '*' ] } });
+    await server.register({
+      plugin: require('../'),
+      options: {
+        host: 'http://localhost:8081',
+        redirectTo: '/test',
+        redirectOnTry: false,
+        strategy: {
+          name: 'micro-auth',
+          mode: 'try'
+        }
+      }
+    });
+
+    server.route({
+      path: '/main',
+      method: 'get',
+      handler(request, h) {
+        // console.log(request.auth);
+        code.expect(request.auth.isAuthorized).to.be.false();
+        return 'Hi There';
+      }
+    });
+
+    await server.start();
+
+    const result = await server.inject({ url: '/main' });
+    code.expect(result.statusCode).to.equal(200);
+
+    await server.stop();
+  });
+
+  lab.test('it should provide getMe function', async() => {
+    const server = new Hapi.Server({ port: 8082, debug: { log: [ '*' ] } });
+    await server.register({
+      plugin: require('../'),
+      options: {
+        host: 'http://localhost:8081',
+        routes: true,
+        cacheEnabled: false
+      }
+    });
+    
+    const user = await server.microauth.getMe('aToken');
+    code.expect(user._id).to.equal('1234');
+  });
+
+  lab.test('it should provide getUser function', async() => {
+    const server = new Hapi.Server({ port: 8082, debug: { log: [ '*' ] } });
+    await server.register({
+      plugin: require('../'),
+      options: {
+        host: 'http://localhost:8081',
+        routes: true,
+        cacheEnabled: false
+      }
+    });
+    
+    const user = await server.microauth.getUser('anotherToken');
+    code.expect(user._id).to.equal('5678');
+  });
+
+  lab.test('it should provide getTokenFromRequest function', async() => {
+    const server = new Hapi.Server({ port: 8082, debug: { log: [ '*' ] } });
+    await server.register({
+      plugin: require('../'),
+      options: {
+        host: 'http://localhost:8081',
+        routes: true,
+        cacheEnabled: false
+      }
+    });
+    
+    const token = server.microauth.getTokenFromRequest({
+      query: {
+        token: 'token1'
+      },
+      state: {
+        cookie: 'cookie'
+      }
+    });
+    code.expect(token).to.equal('token1');
+  });
+
+  lab.test('it should support setCookie', async() => {
+    const server = new Hapi.Server({ port: 8082, debug: { log: [ '*' ] } });
+    await server.register({
+      plugin: require('../'),
+      options: {
+        host: 'http://localhost:8081',
+        routes: true,
+        redirectTo: '/test'
+      }
+    });
+
+    await server.start();
+
+    server.microauth.setCookie({
+      state(cookieName, token, cache) {
+          code.expect(cookieName).to.equal('token');
+          code.expect(token).to.equal('broken');
+          code.expect(cache.ttl).to.equal(2592000000);
+      }
+    }, 'broken');
+
+    await server.stop();
+  });
+
+  lab.test('it should support redirectTo', async() => {
+    const server = new Hapi.Server({ port: 8082, debug: { log: [ '*' ] } });
+    await server.register({
+      plugin: require('../'),
+      options: {
+        host: 'http://localhost:8081',
+        routes: true,
+        redirectTo: '/test'
+      }
+    });
+
+    server.route({
+      path: '/main',
+      method: 'get',
+      handler(r, h) {
+        code.fail('this should be protected');
+        return 'nothing here';
+      }
+    });
+    
+    await server.start();
+
+    const result = await server.inject({ url: '/main' });
+    code.expect(result.statusCode).to.equal(302);
+    code.expect(result.headers.location).to.equal('/test?next=%2Fmain');
+    
+    await server.stop();
+  });
+
+});
